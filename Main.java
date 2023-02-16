14b00d2bf4e9c1238684a19731ead4c75daf8d76
Willian Cordeiro <107742701+darknessbarbie@users.noreply.github.com>
2023-02-16 15:27:02 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+     
+		int X;
+		X = sc.nextInt();
+		System.out.println("The Name Of the best character in 'The Vampire Diaries' is:" + X);
+		
+		sc.close();
+	}
+
+}
